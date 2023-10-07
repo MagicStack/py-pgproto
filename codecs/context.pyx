MagicStack/py-pgproto

commit d1618aa12b5421316ad84e3b3f07ff9275551b2c

Optionally allow JSON encode/decode (#18)

codecs/context.pyx
@@ -12,3 +12,15 @@ cdef class CodecContext:
 
     cdef is_encoding_utf8(self):
         raise NotImplementedError
+
+    cpdef get_json_decoder(self):
+        raise NotImplementedError
+
+    cdef is_decoding_json(self):
+        raise NotImplementedError
+
+    cpdef get_json_encoder(self):
+        raise NotImplementedError
+
+    cdef is_encoding_json(self):
+        raise NotImplementedError

codecs/__init__.pxd
@@ -9,6 +9,10 @@ cdef class CodecContext:
 
     cpdef get_text_codec(self)
     cdef is_encoding_utf8(self)
+    cpdef get_json_decoder(self)
+    cdef is_decoding_json(self)
+    cpdef get_json_encoder(self)
+    cdef is_encoding_json(self)
 
 
 ctypedef object (*encode_func)(CodecContext settings,

codecs/json.pyx
@@ -10,6 +10,9 @@ cdef jsonb_encode(CodecContext settings, WriteBuffer buf, obj):
         char *str
         ssize_t size
 
+    if settings.is_encoding_json():
+        obj = settings.get_json_encoder().encode(obj)
+
     as_pg_string_and_size(settings, obj, &str, &size)
 
     if size > 0x7fffffff - 1:
@@ -26,4 +29,29 @@ cdef jsonb_decode(CodecContext settings, FRBuffer *buf):
     if format != 1:
         raise ValueError('unexpected JSONB format: {}'.format(format))
 
-    return text_decode(settings, buf)
+    rv = text_decode(settings, buf)
+
+    if settings.is_decoding_json():
+        rv = settings.get_json_decoder().decode(rv)
+
+    return rv
+
+
+cdef json_encode(CodecContext settings, WriteBuffer buf, obj):
+    cdef:
+        char *str
+        ssize_t size
+
+    if settings.is_encoding_json():
+        obj = settings.get_json_encoder().encode(obj)
+
+    text_encode(settings, buf, obj)
+
+
+cdef json_decode(CodecContext settings, FRBuffer *buf):
+    rv = text_decode(settings, buf)
+
+    if settings.is_decoding_json():
+        rv = settings.get_json_decoder().decode(rv)
+
+    return rv

commit 1c3cad14d53c8f3088106f4eab8f612b7293569b

Default to not-encoding JSON instead of raising
A regression from #18.

codecs/context.pyx
@@ -17,10 +17,10 @@ cdef class CodecContext:
         raise NotImplementedError
 
     cdef is_decoding_json(self):
-        raise NotImplementedError
+        return False
 
     cpdef get_json_encoder(self):
         raise NotImplementedError
 
     cdef is_encoding_json(self):
-        raise NotImplementedError
+        return False